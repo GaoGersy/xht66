580237997e8d3d8a34a20bf7be8f03e4c29ed4a4
Gersy <gaogersy@163.com>
2017-11-20 22:19:50 +0800

    修改下载文件

--- xht66/pipelines.py
+++ xht66/pipelines.py
@@ -4,25 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import hashlib
+import urllib
 
-import scrapy
 from gevent import os
 from scrapy.utils.project import get_project_settings
 from scrapy.pipelines.images import ImagesPipeline
-from scrapy.utils.python import to_bytes
 
 
 class Xht66Pipeline(ImagesPipeline):
     IMAGES_STORE = get_project_settings().get("IMAGES_STORE")
 
-    def get_media_requests(self, item, info):
-        image_url = item["link"]
-        print(image_url)
-        yield scrapy.Request(image_url)
-
-    def item_completed(self, result, item, info):
-        image_path = [x["path"] for ok, x in result if ok]
+    def process_item(self, item, spider):
         dirName = item['name']
         arr = list(str(item['link']).split("/"))
         imageName = arr[len(arr) - 1]
@@ -30,8 +22,31 @@ class Xht66Pipeline(ImagesPipeline):
         isExists = os.path.exists(path)
         if not isExists:
             os.makedirs(path)
-        os.rename(self.IMAGES_STORE + "/" + image_path[0], path + "/" + imageName)
-
-        item["imagePath"] = self.IMAGES_STORE + "/" + item["name"]
-
+        absoluteSrc= item['link']
+        file_name="%s/%s"%(dirName,imageName)
+        file_path=os.path.join(self.IMAGES_STORE,file_name)
+        print(absoluteSrc)
+        print(file_path)
+        urllib.request.urlretrieve(absoluteSrc,file_path)
         return item
+
+        # def get_media_requests(self, item, info):
+        #     image_url = item["link"]
+        #     print(image_url)
+        #     yield scrapy.Request(image_url)
+        #
+        # def item_completed(self, result, item, info):
+        #     image_path = [x["path"] for ok, x in result if ok]
+        #     print(image_path[0])
+        #     dirName = item['name']
+        #     arr = list(str(item['link']).split("/"))
+        #     imageName = arr[len(arr) - 1]
+        #     path = self.IMAGES_STORE + "/" + dirName
+        #     isExists = os.path.exists(path)
+        #     if not isExists:
+        #         os.makedirs(path)
+        #     os.rename(self.IMAGES_STORE + "/" + image_path[0], path + "/" + imageName)
+        #
+        #     item["imagePath"] = self.IMAGES_STORE + "/" + item["name"]
+        #
+        #     return item
